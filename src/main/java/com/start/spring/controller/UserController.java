c4f5f5222c426077a464f3d3387b1426ff17b4af
salmaghabri <salma.ghabri@insat.ucar.tn>
2024-10-20 16:43:31 +0100

    first commit

--- src/main/java/com/start/spring/controller/UserController.java
+++ src/main/java/com/start/spring/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public String hello(@RequestParam(value = "name", defaultValue = "World !!") String name) {
       return String.format("Hello %s!", name);
     }
 
